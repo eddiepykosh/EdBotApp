b7457409648fd0f5e00962f3eabfd0077127a7ae
Eddie <eddiepykosh@gmail.com>
2022-02-27 18:46:24 -0500

    Make webserver address dynamic

--- EdBotApp/Form1.cs
+++ EdBotApp/Form1.cs
@@ -13,23 +13,56 @@ namespace EdBotApp
 {
     public partial class Form1 : Form
     {
+        public string webServer;
         public Form1()
         {
             InitializeComponent();
+            string promptValue = Prompt.ShowDialog("Web Server", "Please enter your EdBot Server Address");
+            webServer = promptValue;
         }
 
         private void btnSend_Click(object sender, EventArgs e)
         {
+            // MessageBox.Show(webServer);
             string messageContent = txtEntry.Text;
-            Console.WriteLine(messageContent);
-            var responseString = "http://edbot.pykosh.com:1898"
-            .PostStringAsync(messageContent).ReceiveString();
-
-            lblMain.Text = responseString.Result.ToString();
-
+            // Console.WriteLine(messageContent);
+            try
+            {
+                var responseString = webServer
+                .PostStringAsync(messageContent).ReceiveString();
+                lblMain.Text = responseString.Result.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message + " You probably have an invalid web server address.  Restart EdBot.exe and try again");
+            }
 
         }
 
     }
-    
+    public static class Prompt
+    {
+        public static string ShowDialog(string text, string caption)
+        {
+            Form prompt = new Form()
+            {
+                Width = 500,
+                Height = 150,
+                FormBorderStyle = FormBorderStyle.FixedDialog,
+                Text = caption,
+                StartPosition = FormStartPosition.CenterScreen
+            };
+            Label textLabel = new Label() { Left = 50, Top = 20, Text = text };
+            TextBox textBox = new TextBox() { Left = 50, Top = 50, Width = 400 };
+            Button confirmation = new Button() { Text = "Ok", Left = 350, Width = 100, Top = 70, DialogResult = DialogResult.OK };
+            confirmation.Click += (sender, e) => { prompt.Close(); };
+            prompt.Controls.Add(textBox);
+            prompt.Controls.Add(confirmation);
+            prompt.Controls.Add(textLabel);
+            prompt.AcceptButton = confirmation;
+
+            return prompt.ShowDialog() == DialogResult.OK ? textBox.Text : "";
+        }
+    }
+
 }
